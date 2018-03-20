ed55eec7f94df35e360cdd6d7b87da804e7b85ef
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-03-19 21:16:15 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = function (jsobject) {
+    return JSON.stringify(jsobject, null, 2);
+};
---
+++ test/index.js
@@ -0,0 +1,18 @@
+const should = require("chai").should();
+const jsonSort = require("..");
+
+describe("json-sort", () => {
+    it("should sort basic json", () => {
+        const input = {
+            b: "3",
+            a: 2,
+        };
+        const result = jsonSort(input);
+        const expected = `
+{
+  "a": 2,
+  "b": "3"
+}`;
+        result.should.be.eq(expected);
+    });
+});
