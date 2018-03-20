db65b2304775eb71129e8be8c3fd42a7e6664954
Daniel Perez Alvarez <danielpa9708@gmail.com>
2018-03-19 21:21:03 -0400

    Test nested properties

--- test/index.js
+++ test/index.js
@@ -14,4 +14,24 @@ describe("json-sort", () => {
 }`;
         result.should.be.eq(expected);
     });
+    it("should sort json with nested properties", () => {
+        const input = {
+            b: "3",
+            c: {
+                b: 2,
+                a: 1
+            },
+            a: 2,
+        };
+        const result = jsonSort(input);
+        const expected = `{
+  "a": 2,
+  "b": "3",
+  "c": {
+    "a": 1,
+    "b": 2
+  }
+}`;
+        result.should.be.eq(expected);
+    });
 });
